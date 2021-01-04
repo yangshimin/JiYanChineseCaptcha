db525381cf30ba8c1653431d5701dc45e30142cb
Faust Yang <fausty@synnex.com>
2021-01-04 17:30:54 +0800

    用别人训练的模型来代替手动标注数据集

---
+++ config.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 01/04/21
+# @Author  : YangShiMin
+# @Email   : fausty@synnex.com
+# @File    : config.py
+# @Software: PyCharm
+# 验证码图片的宽和高
+CODE_WIDTH, CODE_HEIGHT = 333, 333
+
+# 验证码图片左上角的坐标
+CODE_START_POINT = (615, 421)
+
+# 验证码确认按钮左上角的坐标
+CONFIRM_START_POINT = (826, 763)
