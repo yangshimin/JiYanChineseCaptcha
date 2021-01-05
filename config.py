4f4bf59cac0021218965d00ad63824b40251e1f2
Faust Yang <fausty@synnex.com>
2021-01-05 15:41:59 +0800

    增加配置

--- config.py
+++ config.py
@@ -5,6 +5,12 @@
 # @Email   : fausty@synnex.com
 # @File    : config.py
 # @Software: PyCharm
+import os
+
+# 保存日志的目录
+LOG_PATH = os.getcwd()
+
+
 # 验证码图片的宽和高
 CODE_WIDTH, CODE_HEIGHT = 333, 333
 
@@ -13,3 +19,12 @@ CODE_START_POINT = (615, 421)
 
 # 验证码确认按钮左上角的坐标
 CONFIRM_START_POINT = (826, 763)
+
+# 存放语序图片的目录
+CORNER_IMAGE_DIR = 'sample/image_corner'
+
+# 存放以坐标点命名的图片目录
+POINT_IMAGE_DIR = 'sample/image_point'
+
+# 存放汉字类别的单字图片目录
+SINGLE_CHINESE_IMAGE_DIR = 'sample/image_single_chinese'
